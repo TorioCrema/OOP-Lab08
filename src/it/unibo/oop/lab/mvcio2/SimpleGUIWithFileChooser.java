f3082a4552b033d9c5cb3c3efe05fab655428870
Simone Zama <simone.zama@studio.unibo.it>
2021-11-17 10:00:04 +0100

    Implement SimpleGUIWithFileChooser

--- src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
+++ src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
@@ -1,5 +1,22 @@
 package it.unibo.oop.lab.mvcio2;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import it.unibo.oop.lab.mvcio.Controller;
+
 /**
  * A very simple program using a graphical interface.
  * 
@@ -31,5 +48,75 @@ public final class SimpleGUIWithFileChooser {
      * update the UI: in this example the UI knows when should be updated, so
      * try to keep things separated.
      */
+    private final JFrame frame = new JFrame();
+    private final Controller controller;
 
+    /**
+     * builds a new {@link SimpleGUI}.
+     * @throws IOException 
+     */
+    public SimpleGUIWithFileChooser() throws IOException {
+        this.controller = new Controller();
+        final JPanel canvas = new JPanel();
+        final JPanel upper = new JPanel();
+        upper.setLayout(new BorderLayout());
+        canvas.setLayout(new BorderLayout());
+        final JButton browse = new JButton("Browse...");
+        final JTextField textField = new JTextField(this.controller.getCurrentFilePath());
+        textField.setEditable(false);
+        upper.add(textField, BorderLayout.CENTER);
+        upper.add(browse, BorderLayout.LINE_END);
+        canvas.add(upper, BorderLayout.NORTH);
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        browse.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                final JFileChooser chooser = new JFileChooser();
+                final int retVal = chooser.showSaveDialog(browse);
+                if (retVal == JFileChooser.APPROVE_OPTION) {
+                    try {
+                        SimpleGUIWithFileChooser.this.controller.setCurrentFile(chooser.getSelectedFile());
+                        textField.setText(SimpleGUIWithFileChooser.this.controller.getCurrentFilePath());
+                    } catch (IOException e1) {
+                        JOptionPane.showMessageDialog(frame, e);
+                    }
+                }
+            }
+        });
+
+        /*
+         * Make the frame half the resolution of the screen. This very method is
+         * enough for a single screen setup. In case of multiple monitors, the
+         * primary is selected.
+         * 
+         * In order to deal coherently with multimonitor setups, other
+         * facilities exist (see the Java documentation about this issue). It is
+         * MUCH better than manually specify the size of a window in pixel: it
+         * takes into account the current resolution.
+         */
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / 2, sh / 2);
+        /*
+         * Instead of appearing at (0,0), upper left corner of the screen, this
+         * flag makes the OS window manager take care of the default positioning
+         * on screen. Results may vary, but it is generally the best choice.
+         */
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+    private void display() {
+        this.frame.setVisible(true);
+    }
+
+    public static void main(final String... args) {
+        try {
+            new SimpleGUIWithFileChooser().display();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
